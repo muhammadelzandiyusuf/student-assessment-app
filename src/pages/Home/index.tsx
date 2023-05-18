9dd577fe951d159ad69164e674414bdf78bb5be5
Muhammad Elzandi Yusuf <elzandimilanisty29@gmail.com>
2023-05-18 14:51:53 +0700

    feat(form): add layout form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,55 @@
+import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaUserCircle } from 'react-icons/fa';
+
 import InputNumber from '@/components/InputNumber';
 
+import { FormValueProps } from './types';
+import { FormDefaultValue } from './constans';
+import './style.scss';
+
 const Home = () => {
+
+  const { register, handleSubmit, } = useForm({
+    mode: 'onChange',
+    defaultValues: FormDefaultValue,
+  });
+
+  const onSubmit = useCallback((data: FormValueProps) => {
+    console.log('data', data);
+  }, []);
+
   return (
-    <div>
-      <InputNumber />
+    <div className='container'>
+      <div className='container-wrapper'>
+        <div className='container-title'>
+          <h1>Aplikasi Penilaian Mahasiswa</h1>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='card'>
+            <div className='card-wrapper'>
+              <div className='card-name'>
+                <FaUserCircle size={28} color='#aeb4b7' />
+                <label>Mahasiswa 1</label>
+              </div>
+              <div className='card-form'>
+                <div className='card-form-input'>
+                  <InputNumber />
+                </div>
+                <div className='card-form-input'>
+                  <InputNumber />
+                </div>
+                <div className='card-form-input'>
+                  <InputNumber />
+                </div>
+                <div className='card-form-input'>
+                  <InputNumber />
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
---
+++ src/pages/Home/types.ts
@@ -0,0 +1,50 @@
+export type FormValueProps = {
+  aspek_penilaian_1: {
+    mahasiswa_1: string;
+    mahasiswa_2: string;
+    mahasiswa_3: string;
+    mahasiswa_4: string;
+    mahasiswa_5: string;
+    mahasiswa_6: string;
+    mahasiswa_7: string;
+    mahasiswa_8: string;
+    mahasiswa_9: string;
+    mahasiswa_10: string;
+  },
+  aspek_penilaian_2: {
+    mahasiswa_1: string;
+    mahasiswa_2: string;
+    mahasiswa_3: string;
+    mahasiswa_4: string;
+    mahasiswa_5: string;
+    mahasiswa_6: string;
+    mahasiswa_7: string;
+    mahasiswa_8: string;
+    mahasiswa_9: string;
+    mahasiswa_10: string;
+  },
+  aspek_penilaian_3: {
+    mahasiswa_1: string;
+    mahasiswa_2: string;
+    mahasiswa_3: string;
+    mahasiswa_4: string;
+    mahasiswa_5: string;
+    mahasiswa_6: string;
+    mahasiswa_7: string;
+    mahasiswa_8: string;
+    mahasiswa_9: string;
+    mahasiswa_10: string;
+  },
+  aspek_penilaian_4: {
+    mahasiswa_1: string;
+    mahasiswa_2: string;
+    mahasiswa_3: string;
+    mahasiswa_4: string;
+    mahasiswa_5: string;
+    mahasiswa_6: string;
+    mahasiswa_7: string;
+    mahasiswa_8: string;
+    mahasiswa_9: string;
+    mahasiswa_10: string;
+  },
+};
